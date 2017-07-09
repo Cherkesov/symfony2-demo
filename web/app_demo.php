b53501c6ea7657cb7c34ee908c150b95e06e3f33
GoForBroke <go.for.broke1006@gmail.com>
2017-07-10 02:08:10 +0300

    Setup travis

--- web/app_demo.php
+++ web/app_demo.php
@@ -1,13 +1,14 @@
 <?php
 
-use Symfony\Component\Debug\Debug;
+//use Symfony\Component\Debug\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
 require __DIR__.'/../app/autoload.php';
-Debug::enable();
+//Debug::enable();
 
 $kernel = new AppKernel('demo', true);
 $kernel->loadClassCache();
+
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
