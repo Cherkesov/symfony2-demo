6e221d825a03d3071bf910f80cdfdefc2668200f
GoForBroke <go.for.broke1006@gmail.com>
2017-07-10 02:06:41 +0300

    Setup travis

--- web/app_demo.php
+++ web/app_demo.php
@@ -1,16 +1,13 @@
 <?php
 
+use Symfony\Component\Debug\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
 require __DIR__.'/../app/autoload.php';
-include_once __DIR__.'/../app/bootstrap.php.cache';
+Debug::enable();
 
-$kernel = new AppKernel('demo', false);
+$kernel = new AppKernel('demo', true);
 $kernel->loadClassCache();
-//$kernel = new AppCache($kernel);
-
-// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
-//Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
